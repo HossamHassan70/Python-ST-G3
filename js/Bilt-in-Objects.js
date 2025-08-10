11560d1544e9f48967b226e04831949864bb2c0e
Hossam Hassan <hossam.h.a.hafez@gmail.com>
2025-08-10 13:25:30 +0300

    Upload day-3 of JavaScript files

--- js/Bilt-in-Objects.js
+++ js/Bilt-in-Objects.js
@@ -24,7 +24,49 @@
 // console.log(Math.ceil(2.1)) // 3 -> (2 - 3)
 // console.log(Math.round(2.7)) // 3 -> (2 - 3)
 // console.log(Math.round(2.3)) // 2 -> (2 - 3)
+// ---------------------------------------------
+// Object 
+// const person = {
+//     firstName: "Ahmed",
+//     lastName: "Adel",
+//     age: 50,
+//     eyeColor: "blue",
+//     show: function(name) {
+//         return 'Hello ' + name
+//     }
+// };
+
+// console.log(person['firstName']) // Ahmed
+// console.log(person.lastName) // Adel
+// person.firstName = 'Basmala'
+// console.log(person)
+// console.log(person.show(person.firstName))
+// console.log(person.show('Aya'))
+
+// delete person.eyeColor
+// console.log(person)
+
+// -----------------------------------------------
+// Date() Object 
+
+// const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// var date = new Date()
+// // document.writeln(date)
+// document.writeln(date.getFullYear())
+// document.writeln(date.getMonth())
+// var month = months[date.getMonth()] // getMonth() 0 - 11 
+// console.log(month)
 
 
+// var ul = document.getElementById('demo')
+// const li = document.createElement("li");
+// const li2 = document.createElement("li");
+// const li3 = document.createElement("li");
 
+// li.append(`The Full Year is: ${date.getFullYear()}`)
+// li2.append(date.getMonth())
+// li3.append(date.getHours())
+// ul.append(li,li2,li3)
+// document.writeln(`<br>${date.toLocaleString()}`)
+// document.writeln(`<br>${date.toLocaleTimeString()}`)
 
