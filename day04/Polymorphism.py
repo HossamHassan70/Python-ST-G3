c16e14908c07caaf560e94f6072287365017c6df
Hossam Hassan <hossam.h.a.hafez@gmail.com>
2025-08-21 13:04:27 +0300

    remove python files

--- day04/Polymorphism.py
+++
@@ -1,9 +0,0 @@
-class Math_operation:
-    def __init__(self):
-        pass
-    
-    def summ(self, n1, n2, n3=0, n4=0):
-        return n1 + n2 + n3 + n4
-    
-    # def summ(self, n1, n2, n3):
-    #     return n1 + n2 + n3
