864a5ab1602ca5c5bc111f13cf8edc0686812962
Hossam Hassan <hossam.h.a.hafez@gmail.com>
2025-08-25 10:11:53 +0300

    upload modified files in day 4

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# post (#id, title, description, image, date)
+# Models Benefits (db size, Html widget, validation) 
+class Post(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now=True)
+    
+    # def __str__(self):
+    #     return f'Post: {self.title()}'
+    
